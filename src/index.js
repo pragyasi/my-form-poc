d106adff6d7d7e04d00e1a6df91a7206c8b4cf36
pragya singh <pragya.singh@docusign.com>
2021-08-26 10:07:33 -0700

    added provider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './App';
 
-
+const store = {}
 
 ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
